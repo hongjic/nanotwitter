9f425213315add73203d6df79a97d9abc140ec25
chen hongji <chenhongji@chendeMacBook-Pro.local>
2016-09-30 16:55:14 -0400

    no bug for development_chen

--- config/environments.rb
+++ config/environments.rb
@@ -1,17 +1,12 @@
-require 'zlib'
-
+require 'sinatra'
+require 'active_record'
+require 'yaml'
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
-  # db = URI.parse(ENV['DATABASE_FOR_NANOTWITTER'] || 'postgres://localhost/development')
-  db_env = ENV["DATABASE_FOR_NANOTWITTER"]
-  ActiveRecord::Base.establish_connnection(YAML.load_file(db_env))
-	# ActiveRecord::Base.establish_connection(
-	# 		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-	# 		:host     => db.host,
-	# 		:username => db.user,
-	# 		:password => db.password,
-	# 		:database => db.path[1..-1],
-	# 		:encoding => 'utf8'
-	# )
+  #db_env = ENV["DATABASE_FOR_NANOTWITTER"]
+  db_env = "development_sam"
+  puts "[development environment: #{db_env}]"
+  database = YAML.load_file("config/database.yml")
+  ActiveRecord::Base.establish_connection(database[db_env])
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,85 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930041025) do
+ActiveRecord::Schema.define(version: 20160930040949) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "tweet_id",                  null: false
+    t.integer  "user_id",                   null: false
+    t.text     "content",     limit: 65535, null: false
+    t.datetime "create_time",               null: false
+    t.index ["tweet_id"], name: "tweet_id_in_comments_idx", using: :btree
+    t.index ["user_id"], name: "user_id_in_comments_idx", using: :btree
+  end
 
-  create_table "followers", force: :cascade do |t|
+  create_table "followers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "follower_id"
     t.integer  "user_id"
     t.datetime "create_time"
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "tweet_id"
-    t.integer "user_id"
+  create_table "follows", primary_key: ["follower_id", "followed_id"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "follower_id", null: false
+    t.integer  "followed_id", null: false
+    t.datetime "create_time", null: false
+    t.index ["followed_id"], name: "followed_id_idx", using: :btree
+    t.index ["follower_id"], name: "follower_id_idx", using: :btree
   end
 
-  create_table "marks", force: :cascade do |t|
-    t.integer "tweet_id"
-    t.integer "tag_id"
+  create_table "likes", primary_key: ["user_id", "tweet_id"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id",  null: false
+    t.integer "tweet_id", null: false
+    t.index ["tweet_id"], name: "tweet_id_in_likes_idx", using: :btree
+    t.index ["user_id"], name: "user_id_in_likes_idx", using: :btree
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "name"
+  create_table "notifications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "target_user_id",                              null: false
+    t.integer "tweet_id",                                    null: false
+    t.string  "status",         limit: 6, default: "unread", null: false
+    t.string  "type",           limit: 7,                    null: false
+    t.integer "action_user_id",                              null: false
+    t.index ["action_user_id"], name: "action_user_id_idx", using: :btree
+    t.index ["target_user_id"], name: "target_user_id_idx", using: :btree
+    t.index ["tweet_id"], name: "tweet_id_idx", using: :btree
   end
 
-  create_table "notifications", force: :cascade do |t|
-    t.integer "notification_id"
-    t.integer "target_user_id"
-    t.integer "action_user_id"
-    t.integer "tweet_id"
-    t.boolean "status"
-    t.boolean "type"
+  create_table "tags", primary_key: ["tag_name", "tweet_id"], force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string  "tag_name", limit: 20, null: false
+    t.integer "tweet_id",            null: false
+    t.index ["tag_name"], name: "tag_name_idx", using: :btree
+    t.index ["tweet_id"], name: "tweet_id_idx", using: :btree
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.integer "tweet_id"
-    t.string  "tag_name"
+  create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",                               null: false
+    t.integer  "likes",                     default: 0, null: false
+    t.text     "content",     limit: 65535,             null: false
+    t.datetime "create_time",                           null: false
+    t.integer  "comments",                  default: 0, null: false
+    t.index ["user_id"], name: "user_id_idx", using: :btree
   end
 
-  create_table "tweets", force: :cascade do |t|
-    t.integer  "tweet_id"
-    t.integer  "user_id"
-    t.datetime "create_time"
-    t.string   "content"
-    t.integer  "likes"
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",        limit: 20,                     null: false
+    t.string   "email",       limit: 45,                     null: false
+    t.string   "passwd",      limit: 20,                     null: false
+    t.datetime "create_time",                                null: false
+    t.string   "gender",      limit: 7,  default: "unknown", null: false
+    t.datetime "birthday"
+    t.index ["email"], name: "email_UNIQUE", unique: true, using: :btree
+    t.index ["name"], name: "name_UNIQUE", unique: true, using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "create_time"
-    t.string   "gender"
-    t.date     "birthday"
-  end
-
+  add_foreign_key "comments", "tweets", name: "tweet_id_in_comments"
+  add_foreign_key "comments", "users", name: "user_id_in_comments"
+  add_foreign_key "follows", "users", column: "followed_id", name: "followed_id", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "follows", "users", column: "follower_id", name: "follower_id", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "likes", "tweets", name: "tweet_id_in_likes"
+  add_foreign_key "likes", "users", name: "user_id_in_likes"
+  add_foreign_key "notifications", "tweets", name: "tweet_id_in_notifications"
+  add_foreign_key "notifications", "users", column: "action_user_id", name: "action_user_id"
+  add_foreign_key "notifications", "users", column: "target_user_id", name: "target_user_id"
+  add_foreign_key "tags", "tweets", name: "tweet_id_in_tags", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "tweets", "users", name: "user_id", on_update: :cascade, on_delete: :cascade
 end
