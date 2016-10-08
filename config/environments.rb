941110c995020305482a176e3133d061ddae3598
Hongji Chen <chjre2015@gmail.com>
2016-10-08 15:19:01 -0400

    configure production database connection

--- config/environments.rb
+++ config/environments.rb
@@ -16,3 +16,17 @@ configure :development do
     :pool => 5
     )
 end
+
+configure :production do
+  puts "[development environment: production]"
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8',
+    :pool => 5
+    )
+end
